e62229d1c74ee3d8332fdeec3b4312606dc6388f
hafizarr <hafizarr.98@gmail.com>
2021-09-09 20:53:47 +0700

    auth middleware

--- handler/user.go
+++ handler/user.go
@@ -135,7 +135,8 @@ func (h *userHandler) UploadAvatar(c *gin.Context) {
 		return
 	}
 
-	userID := 1 // Nantinya akan dapet dari JWT
+	currentUser := c.MustGet("currentUser").(user.User)
+	userID := currentUser.ID
 
 	path := fmt.Sprintf("images/%d-%s", userID, file.Filename)
 
